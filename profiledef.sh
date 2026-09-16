1376407d2f9d1c1ea0adf14d55c19e55ad492afc
Eto <xyuhn23@gmail.com>
2026-09-16 18:51:14 +0700

    feat: initial NyxOS custom archiso profile (CachyOS kernel + Hyprland + Limine + Caelestia shell)

---
+++ profiledef.sh
@@ -0,0 +1,18 @@
+iso_name="nyxos"
+iso_label="NYXOS_$(date +%Y%m)"
+iso_version="$(date +%Y.%m.%d)"
+install_dir="arch"
+buildmodes=('iso')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.limine.esp')
+arch="x86_64"
+pacman_conf="pacman.conf"
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '3')
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/root"]="0:0:750"
+  ["/root/.gnupg"]="0:0:700"
+  ["/usr/local/bin/choose-mirror"]="0:0:755"
+  ["/usr/local/bin/Installation_guide"]="0:0:755"
+  ["/usr/local/bin/livecd-sound"]="0:0:755"
+)
